6b06c58cc196bd8c06a01db85ade637a6d8418e9
Aaron Davis <aaron.m.davis@comcast.net>
2025-11-10 11:58:00 -0600

    added cachix for ros

--- configuration.nix
+++ configuration.nix
@@ -53,6 +53,8 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  nix.settings.substituters = [ "https://cache.nixos.org" "https://ros.cachix.org" ];
+  nix.settings.trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo=" ];
   system.stateVersion = "25.05";
 
 }
